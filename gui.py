9ac06efad90dcfbdcdb2de713c452ff3a5f4ebe9
Charlie Franzel <charliefranzel@gmail.com>
2025-02-18 15:10:03 -0700

    Revert changes

--- gui.py
+++ gui.py
@@ -15,34 +15,29 @@ window.resizable(False, False)
 window.title("Chat Room")
 window.geometry("450x400")
 
-frame = tk.Frame(window)
-frame.pack(expand=True)  # Centers the frame automatically
-
-title = tk.Label(frame, text="Chat Room")
-namelabel = tk.Label(frame, text="Enter Name")
-nameentry = tk.Entry(frame, width= 20)
-iplabel = tk.Label(frame, text="Enter IP")
-ipentry = tk.Entry(frame, width = 16)
-portlabel = tk.Label(frame, text="Port")
-portentry = tk.Entry(frame, width = 6)
-submit = tk.Button(
-    frame, 
+title = tk.Label(text="Chat Room")
+namelabel = tk.Label(text="Enter Name")
+nameentry = tk.Entry(width= 20)
+iplabel = tk.Label(text="Enter IP")
+ipentry = tk.Entry(width = 16)
+portlabel = tk.Label(text="Port")
+portentry = tk.Entry(width = 6)
+submit = tk.Button( 
     text="Submit", 
     command=submit_name
     )
-output = tk.Label(
-    frame, 
+output = tk.Label( 
     text="",
     foreground="red"
 )
 
-title.grid(row=0, column=0)
-#namelabel.grid(row=1, column=0)
-nameentry.grid(row=1, column=0)
-#iplabel.grid(row=2, column=0)
-ipentry.grid(row=2, column=0)
-#portlabel.grid(row=3, column=0)
-portentry.grid(row=2, column=1)
-submit.grid(row=4, column=0)
-output.grid(row=5, column=0)
+title.pack()
+namelabel.pack()
+nameentry.pack()
+iplabel.pack()
+ipentry.pack()
+portlabel.pack()
+portentry.pack()
+submit.pack()
+output.pack()
 window.mainloop()
